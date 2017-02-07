2413746130be177c26f93c914c7c5a0838b52961
dholmes <dholmes@NAVL40326>
2017-02-07 08:26:37 -0700

    Updating for api data template

--- src/Website/js/main.js
+++ src/Website/js/main.js
@@ -1,1 +1,13 @@
-﻿
+﻿(function() {
+
+    //Documentation at: http://navitaireinterviewapp.azurewebsites.net/swagger/ui/index.html
+
+    var flightsUrl = 'http://navitaireinterviewapp.azurewebsites.net/api/Flights';
+
+    $.get(flightsUrl, function (data) {
+
+        $('#data').html(data);
+
+    });
+
+})();
