268b2e823058c9d2c819d870b78b41e0ef7a6066
dholmes <dholmes@NAVL40326>
2017-02-09 09:59:11 -0700

    solution finished

--- src/Website/js/main.js
+++ src/Website/js/main.js
@@ -6,6 +6,21 @@
 
     $.get(flightsUrl, function (data) {
 
+        data.forEach(function(item) {
+
+            var date = new Date(item.departure);
+
+            item.departure = (date.getMonth() + 1) +
+                '-' +
+                date.getDate() +
+                '-' +
+                date.getFullYear() +
+                ' ' +
+                ("0" + date.getHours()).slice(-2) +
+                ":" +
+                ("0" + date.getMinutes()).slice(-2);
+        });
+
         var source = $("#entry-template").html();
         var template = Handlebars.compile(source);
         var html = template(data);
